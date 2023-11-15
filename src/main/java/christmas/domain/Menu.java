bc4fe7e4e9f80a489c9be72d47dc05671f4350fc
mac <seunghune12@gmail.com>
2023-11-15 13:22:48 +0900

    feat: enum을 주문한 총 메뉴의 총가격 계산 구현

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -15,5 +15,14 @@ public class Menu {
         this.menus = MenuSplitor.splitMenus(order);
     }
 
+    public void calculateTotalPrice() {
+        for (Map.Entry<String, Integer> entry : menus.entrySet()) {
+            String menuName = entry.getKey();
+            int quantity = entry.getValue();
 
+            // FoodName 열거형에서 해당 메뉴의 가격을 가져와서 총 가격에 더함
+            int menuPrice = FoodName.valueOf(menuName).getPrice();
+            price += menuPrice * quantity;
+        }
+    }
 }
